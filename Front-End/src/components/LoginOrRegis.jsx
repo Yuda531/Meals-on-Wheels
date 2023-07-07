7097e05527ca72ed33f41442e54052fd84119d34
Yuda531 <yudaagung70@gmail.com>
2023-07-07 09:36:16 +0700

    fixed admin

--- Front-End/src/components/LoginOrRegis.jsx
+++ Front-End/src/components/LoginOrRegis.jsx
@@ -106,102 +106,116 @@ const LoginOrRegis = () => {
   };
 
   return (
-    <div className="col-6">
+    <div className="col-lg-6">
       {!showReplacement ? (
         <div id="regisForm">
           <h1 className="display-6 text-white">Sign Up</h1>
           <hr className="border-white" />
-          <form className="col-12" onSubmit={handleSubmit}>
-            <div className="form-floating mb-3">
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                placeholder="Name"
-                value={name}
-                onChange={(e) => setUserName(e.target.value)}
-                required
-              />
-              <label htmlFor="name">Name</label>
+          <form className="row g-3" onSubmit={handleSubmit}>
+            <div className="col-12 col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setUserName(e.target.value)}
+                  required
+                />
+                <label htmlFor="name">Name</label>
+              </div>
             </div>
-            <div className="form-floating mb-3">
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="Email address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <label htmlFor="email">Email address</label>
+            <div className="col-12 col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <label htmlFor="email">Email address</label>
+              </div>
             </div>
-            <div className="form-floating mb-3">
-              <select
-                placeholder="Select a Role."
-                id="roleId"
-                className="form-control"
-                value={loginRole}
-                onChange={(e) => setRole(e.target.value)}
-                required
-              >
-                <option value="" disabled>
-                  Select a Role
-                </option>
-                <option value="Partner">Partner</option>
-                <option value="Member">Member</option>
-                <option value="Caregiver">Caregiver</option>
-                <option value="Donor">Donor</option>
-                <option value="Volunteer">Volunteer</option>
-              </select>
-              <label htmlFor="roleId">Which role do you want to be?</label>
+            <div className="col-12">
+              <div className="form-floating mb-3">
+                <select
+                  placeholder="Select a Role."
+                  id="roleId"
+                  className="form-control"
+                  value={loginRole}
+                  onChange={(e) => setRole(e.target.value)}
+                  required
+                >
+                  <option value="" disabled>
+                    Select a Role
+                  </option>
+                  <option value="Partner">Partner</option>
+                  <option value="Member">Member</option>
+                  <option value="Caregiver">Caregiver</option>
+                  <option value="Donor">Donor</option>
+                  <option value="Volunteer">Volunteer</option>
+                </select>
+                <label htmlFor="roleId">Which role do you want to be?</label>
+              </div>
             </div>
-            <div className="form-floating mb-3">
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <label htmlFor="password">Password</label>
+            <div className="col-12 col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <label htmlFor="password">Password</label>
+              </div>
             </div>
-            <div className="form-floating mb-3">
-              <Form.Control
-                type="password"
-                className="form-control"
-                id="Cfpassword"
-                value={confirmPassword}
-                placeholder="Confirm Password"
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-                isInvalid={passwordError}
-              />
-              <Form.Label htmlFor="Cfpassword">Confirm Password</Form.Label>
-              {passwordError && (
-                <Form.Control.Feedback type="invalid">
-                  Passwords do not match
-                </Form.Control.Feedback>
-              )}
+            <div className="col-12 col-md-6">
+              <div className="form-floating mb-3">
+                <Form.Control
+                  type="password"
+                  className="form-control"
+                  id="Cfpassword"
+                  value={confirmPassword}
+                  placeholder="Confirm Password"
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                  isInvalid={passwordError}
+                />
+                <Form.Label htmlFor="Cfpassword">Confirm Password</Form.Label>
+                {passwordError && (
+                  <Form.Control.Feedback type="invalid">
+                    Passwords do not match
+                  </Form.Control.Feedback>
+                )}
+              </div>
             </div>
 
-            <p className="lead text-white">
-              Already have an account? Click{" "}
-              <span>
-                <a className="text-warning" onClick={handleClick}>
-                  here to Sign-In
-                </a>
-              </span>
-            </p>
-            <button
-              type="submit"
-              className="btn btn-outline-warning col-6 my-3"
-            >
-              Register
-            </button>
-            <br />
+            <div className="col-12">
+              <p className="lead text-white">
+                Already have an account? Click{" "}
+                <span>
+                  <a className="text-warning" onClick={handleClick}>
+                    here to Sign-In
+                  </a>
+                </span>
+              </p>
+            </div>
+
+            <div className="col-12">
+              <button
+                type="submit"
+                className="btn btn-outline-warning col-12 my-3"
+              >
+                Register
+              </button>
+            </div>
           </form>
         </div>
       ) : (
@@ -209,51 +223,57 @@ const LoginOrRegis = () => {
         <div id="loginForm">
           <h1 className="display-6 text-white">Sign In</h1>
           <hr className="border-white" />
-          <form onSubmit={handleLoginFormSubmit} className="col-12">
-            <div className="form-floating mb-3">
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="Email address"
-                value={loginName}
-                onChange={(e) => setLoginName(e.target.value)}
-                required
-              />
-              <label htmlFor="email">Email address</label>
+          <form onSubmit={handleLoginFormSubmit} className="row g-3">
+            <div className="col-12">
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Email address"
+                  value={loginName}
+                  onChange={(e) => setLoginName(e.target.value)}
+                  required
+                />
+                <label htmlFor="email">Email address</label>
+              </div>
             </div>
-            <div className="form-floating mb-3">
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                placeholder="Password"
-                value={loginPassword}
-                onChange={(e) => setPasswordLogin(e.target.value)}
-                required
-              />
-              <label htmlFor="password">Password</label>
+            <div className="col-12">
+              <div className="form-floating mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  value={loginPassword}
+                  onChange={(e) => setPasswordLogin(e.target.value)}
+                  required
+                />
+                <label htmlFor="password">Password</label>
+              </div>
             </div>
-            <p className="lead text-white mt-3">
-              Don't have an account? Click{" "}
-              <span>
-                <a className="text-warning" onClick={handleBack}>
-                  here to Sign-Up
-                </a>
-              </span>
-            </p>
-
-            <button
-              type="submit"
-              className="btn btn-outline-warning col-6 my-3"
-            >
-              Login
-            </button>
+            <div className="col-12">
+              <p className="lead text-white mt-3">
+                Don't have an account? Click{" "}
+                <span>
+                  <a className="text-warning" onClick={handleBack}>
+                    here to Sign-Up
+                  </a>
+                </span>
+              </p>
+            </div>
+            <div className="col-12">
+              <button
+                type="submit"
+                className="btn btn-outline-warning col-12 my-3"
+              >
+                Login
+              </button>
+            </div>
           </form>
         </div>
       )}
     </div>
-
   );
 };
 
