f93795229f268d5153ad7561b6a457ac54b69925
Mkemalz <mterlibusokemal@gmail.com>
2023-07-07 11:05:22 +0700

    responsive donor dan contact

--- Front-End/src/Pages/Donor.jsx
+++ Front-End/src/Pages/Donor.jsx
@@ -1,4 +1,4 @@
-import background from '../img/background.jpeg'
+import background from '../img/sedekah1.jpg'
 import StickyHeader from '../components/header';
 
 const Donor = () => {
@@ -104,109 +104,95 @@ const Donor = () => {
 
 
 
+    /////
 
-
-
-
-
-
-      <div className="d-flex align-items-center justify-content-center  " style={{backgroundImage: `url(${background})`}}>
-        <div className="container">
-          <div className="row">
-            <div className="col-8 p-5 m-5 bg-white">
+    <div className="d-flex align-items-center justify-content-center mt-4" style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-8 p-5 m-5 bg-white">
             <StickyHeader activePage={"contact-us"} />
-              <h1>
-                <span style={{ color: "red" }}>Indonesia</span> Senior need you
-              </h1>
-              <h3>
-                Every knock delivers the food, compassion and care that our aging
-                neighbors need.
-              </h3>
+            <h1>
+              <span style={{ color: "red" }}>Indonesia</span> Senior need you
+            </h1>
+            <h3>
+              Every knock delivers the food, compassion and care that our aging neighbors need.
+            </h3>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-8 p-5 m-5 bg-white text-center d-flex flex-column">
+            <h1>Donate Now</h1>
+            <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="btnradio1"
+                autoComplete="off"
+              />
+              <label className="btn btn-outline-primary" htmlFor="btnradio1">
+                $10
+              </label>
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="btnradio2"
+                autoComplete="off"
+              />
+              <label className="btn btn-outline-primary" htmlFor="btnradio2">
+                $15
+              </label>
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="btnradio3"
+                autoComplete="off"
+              />
+              <label className="btn btn-outline-primary" htmlFor="btnradio3">
+                $20
+              </label>
             </div>
-  
-            <div className="col-8 p-5 m-5 bg-white text-center d-flex flex-column">
-              <h1>Donation Now</h1>
-  
-              <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
-                <input
-                  type="radio"
-                  className="btn-check"
-                  name="btnradio"
-                  id="btnradio1"
-                  autoComplete="off"
-                />
-                <label className="btn btn-outline-primary" htmlFor="btnradio1">
-                  $10
-                </label>
-  
-                <input
-                  type="radio"
-                  className="btn-check"
-                  name="btnradio"
-                  id="btnradio2"
-                  autoComplete="off"
-                />
-                <label className="btn btn-outline-primary" htmlFor="btnradio2">
-                  $15
-                </label>
-  
-                <input
-                  type="radio"
-                  className="btn-check"
-                  name="btnradio"
-                  id="btnradio3"
-                  autoComplete="off"
-                />
-                <label className="btn btn-outline-primary" htmlFor="btnradio3">
-                  $20
-                </label>
-              </div>
-  
-              <div className="btn-group mt-4" role="group" aria-label="Basic radio toggle button group">
-                <input
-                  type="radio"
-                  className="btn-check"
-                  name="btnradio"
-                  id="btnradio4"
-                  autoComplete="off"
-                />
-                <label className="btn btn-outline-primary" htmlFor="btnradio4">
-                  $25
-                </label>
-  
-                <input
-                  type="radio"
-                  className="btn-check"
-                  name="btnradio"
-                  id="btnradio5"
-                  autoComplete="off"
-                />
-                <label className="btn btn-outline-primary" htmlFor="btnradio5">
-                  $50
-                </label>
-  
-                <input
-                  type="radio"
-                  className="btn-check"
-                  name="btnradio"
-                  id="btnradio6"
-                  autoComplete="off"
-                />
-                <label className="btn btn-outline-primary" htmlFor="btnradio6">
-                  $100
-                </label>
-              </div>
-              <button type="button" className="btn btn-primary btn-sm mt-4">
-                Donate
-              </button>
+            <div className="btn-group mt-4" role="group" aria-label="Basic radio toggle button group">
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="btnradio4"
+                autoComplete="off"
+              />
+              <label className="btn btn-outline-primary" htmlFor="btnradio4">
+                $25
+              </label>
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="btnradio5"
+                autoComplete="off"
+              />
+              <label className="btn btn-outline-primary" htmlFor="btnradio5">
+                $50
+              </label>
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="btnradio6"
+                autoComplete="off"
+              />
+              <label className="btn btn-outline-primary" htmlFor="btnradio6">
+                $100
+              </label>
             </div>
+            <button type="button" className="btn btn-primary btn-sm mt-4">
+              Donate
+            </button>
           </div>
         </div>
       </div>
-   
-  
-
-
+    </div>
 
   );
 };
