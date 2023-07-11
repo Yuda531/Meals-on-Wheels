71044017f1d5341cc17ace8a6a85178be0a20fb3
Yuda531 <yudaagung70@gmail.com>
2023-07-11 18:12:14 +0700

    hapus backend dulu

--- Back-End/SAModule9V2/src/main/java/com/example/mow/interceptor/InvalidPasswordException.java
+++
@@ -1,8 +0,0 @@
-package com.example.mow.interceptor;
-
-public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
-
