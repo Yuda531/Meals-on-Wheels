0ecbcaebdbddca63285c597d0935906ae54dd6b0
Yuda531 <yudaagung70@gmail.com>
2023-07-11 18:18:28 +0700

    Remove all contents of master branch

--- Front-End/src/Pages/MemberDashboard.jsx
+++
@@ -1,7 +0,0 @@
-import HeroMember from "../component/HeroMember";
-import MemberContent from "../component/MemberContent";
-
-function MemberDashboard(params) {
-  return <MemberContent></MemberContent>;
-}
-export default MemberDashboard;
