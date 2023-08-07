e1ddec71a95f19fd8d42222f552fb6493814021a
rockyRS2 <rockyramdhansetiawan@gmail.com>
2023-08-07 22:53:56 +0700

    reponsive dikit

--- Front-End/src/components/RegisLogin.jsx
+++ Front-End/src/components/RegisLogin.jsx
@@ -3,22 +3,21 @@ import LoginOrRegis from "./LoginOrRegis";
 function RegisLogin(){
     return(
         <div className="body">
-            <div className="backblur">
-                <div className="col-12 d-flex">
-                    <div className="col-3 my-auto">
-                        <h1 className="display-2 fw-bold text-white">
-                            <span className="text-warning">Sign-In </span>
-                                or 
-                                <br /><span className="text-success">Sign-Up</span>
-                        </h1>
-                        
-                    </div>
-                    <div className="aboutt ms-4 col-1"></div>
-
-                    <LoginOrRegis />
-                </div>
+        <div className="backblur">
+          <div className="col-12 d-flex">
+            <div className="col-md-3 col-12 my-auto">
+              <h1 className="display-2 fw-bold text-white">
+                <span className="text-warning">Sign-In </span>
+                or 
+                <br /><span className="text-success">Sign-Up</span>
+              </h1>
             </div>
+            <div className="aboutt ms-4 col-md-1 col-12 my-auto"></div>
+            <LoginOrRegis />
+          </div>
         </div>
+      </div>
+      
     )
 }
 
--- Front-End/src/components/landingNoLogin.jsx
+++ Front-End/src/components/landingNoLogin.jsx
@@ -30,7 +30,7 @@ function LandingNoLogin() {
           style={{ paddingTop: "6%", paddingBottom: "6%" }}
           className=" col-12"
         >
-          <div className="d-flex col-12">
+          <div className="d-flex col-12 row">
             <div className="col-6 p-3">
               <h1
                 style={{ fontSize: "85px" }}
@@ -74,84 +74,82 @@ function LandingNoLogin() {
         <div style={{ padding: "0.25%" }} className="bg-white"></div>
 
         <div className="backblur2">
-          <div className="px-5 d-flex justify-content-center">
-            <h1 className="display-3 mb-5 my-auto">
-              Why <span className="text-success fw-bold">Meals </span> on
-              <span className="text-warning fw-bold"> Wheels</span> ?
-            </h1>
-          </div>
+  <div className="px-5 d-flex justify-content-center">
+    <h1 className="display-3 mb-5 my-auto">
+      Why <span className="text-success fw-bold">Meals </span> on
+      <span className="text-warning fw-bold"> Wheels</span> ?
+    </h1>
+  </div>
 
-          <div className="col-12 d-flex justify-content-center">
-            <div className="card rounded-5 p-5 col-3 whyUs mx-3">
-              <img src={deliver} className="col-12 mx-auto" alt="" />
-              <h1
-                style={{ fontSize: "30px" }}
-                className="text-center fw-bold mt-2"
-              >
-                <span className="text-success">On</span>-time{" "}
-                <span className="text-warning">Delivery</span>
-              </h1>
-            </div>
+  <div className="row justify-content-center">
+    <div className="col-md-4 col-sm-6 mb-4">
+      <div className="card rounded-5 p-5 whyUs">
+        <img src={deliver} className="col-12 mx-auto" alt="" />
+        <h1 style={{ fontSize: "30px" }} className="text-center fw-bold mt-2">
+          <span className="text-success">On</span>-time{" "}
+          <span className="text-warning">Delivery</span>
+        </h1>
+      </div>
+    </div>
 
-            <div className="card rounded-5 p-5 col-3 whyUs mx-3">
-              <img src={healthy} className="col-12 mx-auto" alt="" />
-              <h1
-                style={{ fontSize: "30px" }}
-                className="text-center  fw-bold mt-2"
-              >
-                <span className="text-success">Healthy</span> food{" "}
-                <span className="text-warning">Quality</span>
-              </h1>
-            </div>
+    <div className="col-md-4 col-sm-6 mb-4">
+      <div className="card rounded-5 p-5 whyUs">
+        <img src={healthy} className="col-12 mx-auto" alt="" />
+        <h1 style={{ fontSize: "30px" }} className="text-center fw-bold mt-2">
+          <span className="text-success">Healthy</span> food{" "}
+          <span className="text-warning">Quality</span>
+        </h1>
+      </div>
+    </div>
 
-            <div className="card rounded-5 p-5 col-3 whyUs mx-3">
-              <img src={afford} className="col-12 mx-auto" alt="" />
-              <h1
-                style={{ fontSize: "30px" }}
-                className="text-center  fw-bold mt-2"
-              >
-                <span className="text-success">Affordable</span> food{" "}
-                <span className="text-warning">Price</span>
-              </h1>
-            </div>
-          </div>
-        </div>
+    <div className="col-md-4 col-sm-6 mb-4">
+      <div className="card rounded-5 p-5 whyUs">
+        <img src={afford} className="col-12 mx-auto" alt="" />
+        <h1 style={{ fontSize: "30px" }} className="text-center fw-bold mt-2">
+          <span className="text-success">Affordable</span> food{" "}
+          <span className="text-warning">Price</span>
+        </h1>
+      </div>
+    </div>
+  </div>
+</div>
+
         <div style={{ padding: "0.25%" }} className="bg-white"></div>
       </div>
 
       <div className="backblur1">
-        <div style={{ padding: "0%" }} className=" col-12">
-          <div className="d-flex col-12  my-auto">
-            <div
-              style={{ paddingLeft: "10%", paddingRight: "5%" }}
-              className="col-9 my-auto"
-            >
-              {/* <img style={{borderRadius:"23% 40% 22% 60%"}} src={deliver} className="col-10" alt="" /> */}
-              <h1
-                style={{ fontSize: "100px" }}
-                className="text-white fw-bold col-12 my-auto"
-              >
-                Fast <span className=" text-success"> and </span>
-                <br />
-                <br />
-                <span className="text-warning">Satisfying.</span>
-              </h1>
-              <br />
-              <br />
-              <h4 className="text-white lead col-6 my-4">
-                Guaranteed{" "}
-                <span className="text-warning fw-bold">Satisfaction</span>, On-
-                <span className="fw-bold text-success">time</span> delivery, and{" "}
-                <span className="fw-bold text-success">Healthy</span>
-              </h4>
-              <button className="btn btn-success col-6 my-3">Order Now</button>
-            </div>
-            <div className="col-3">
-              <img src={foods} className="col-12 mx-auto" alt="" />
-            </div>
-          </div>
-        </div>
+  <div style={{ padding: "0%" }} className="col-12">
+    <div className="row">
+      <div
+        style={{ paddingLeft: "10%", paddingRight: "5%" }}
+        className="col-12 col-md-9 my-auto"
+      >
+        <h1
+          style={{ fontSize: "65px" }}
+          className="text-white fw-bold col-12 my-auto"
+        >
+          Fast <span className="text-success">and</span>
+          <br />
+          <span className="text-warning">Satisfying.</span>
+        </h1>
+        <h4 className="text-white lead col-12 col-md-6 my-4">
+          Guaranteed <span className="text-warning fw-bold">Satisfaction</span>,
+          On-<span className="text-success fw-bold">time</span> delivery, and{" "}
+          <span className="text-success fw-bold">Healthy</span>
+        </h4>
+        <button className="btn btn-success col-6 my-3">Order Now</button>
       </div>
+      <div className="col-12 col-md-3 my-auto">
+        <img
+          src={foods}
+          className="col-12 mx-auto d-block img-fluid"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>
   );
 }
