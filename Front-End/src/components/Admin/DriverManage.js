5bf5eb4c5fc92589877ba4f142f47e84476a91fd
Yuda531 <yudaagung70@gmail.com>
2023-08-09 09:14:17 +0700

    edit layout admin

--- Front-End/src/components/Admin/DriverManage.js
+++ Front-End/src/components/Admin/DriverManage.js
@@ -167,13 +167,6 @@ const DriverManage = () => {
                               <td>{driverReq.vehicle}</td>
                               <td>
                                 <button
-                                  className="btn btn-secondary rounded-2 ms-2"
-                                  data-bs-toggle="modal"
-                                  data-bs-target="#deleteMenu"
-                                >
-                                  Details <i className="fas fa-eye fa-sm"></i>
-                                </button>
-                                <button
                                   className="btn btn-danger rounded-2 ms-2"
                                   data-bs-toggle="modal"
                                   data-bs-target="#editMenu"
