9caa3eae4414c0d5c05decfb556d7e7101fa0e38
Yuda531 <yudaagung70@gmail.com>
2023-08-09 18:45:20 +0700

    Admin Dash Fix

--- Front-End/src/components/Admin/AdminDash.js
+++ Front-End/src/components/Admin/AdminDash.js
@@ -1,17 +1,78 @@
 // AdminDashboard.js
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect} from "react";
+import axios from "axios";
 import "../../CSS/admin/AdminDashboard.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import backgroundImage from "../../images/bg/diamond_upholstery.png";
 
 const AdminDashboard = () => {
-  // Dummy data for current statistics
-  const currentDonationAmount = "5000";
-  const jumlahMembers = 15;
-  const jumlahDrivers = 20;
-  const jumlahPartners = 10;
 
+  const [totalDonation, setTotalDonation] = useState(0);
+  const [totalMembers, setTotalMembers] = useState(0);
+  const [totalDrivers, setTotalDrivers] = useState(0);
+  const [totalPartners, setTotalPartners] = useState(0);
+
+
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/admin/all-donate")
+      .then(response => {
+        const donationData = response.data;
+        let totalAmount = 0;
+  
+        // Menghitung total donasi dari respons
+        donationData.forEach(donation => {
+          totalAmount += donation.donate_amount;
+        });
+  
+        setTotalDonation(totalAmount);
+      })
+      .catch(error => {
+        console.error("Error fetching donation data:", error);
+      });
+  }, []);
+
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/admin/all-members")
+      .then(response => {
+        const membersData = response.data;
+        const totalMembers = membersData.length;
+  
+        setTotalMembers(totalMembers);
+      })
+      .catch(error => {
+        console.error("Error fetching members data:", error);
+      });
+  }, []);
+  
+  useEffect(() => {
+    axios.get("http://localhost:8080/admin/caregivers/active") // Ubah URL sesuai endpoint yang benar
+      .then(response => {
+        const driversData = response.data;
+        const totalDrivers = driversData.length; // Menghitung total driver dari respons
+        setTotalDrivers(totalDrivers);
+      })
+      .catch(error => {
+        console.error("Error fetching drivers data:", error);
+      });
+  }, []);
+
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/admin/partners/active") // Ubah URL sesuai endpoint yang benar
+      .then(response => {
+        const partnersData = response.data;
+        const totalPartners = partnersData.length; // Menghitung total partners dari respons
+        setTotalPartners(totalPartners);
+      })
+      .catch(error => {
+        console.error("Error fetching partners data:", error);
+      });
+
+    // ...
+  }, []);
  
 
   return (
@@ -34,7 +95,7 @@ const AdminDashboard = () => {
                     Current Donation Amount
                   </h5>
                   <p className="card-text text-success">
-                    <i class="fa fa-usd"></i> {currentDonationAmount}{" "}
+                    <i class="fa fa-usd"></i> {totalDonation}
                   </p>
                 </div>
                 <div className="card-footer d-flex align-items-center justify-content-between">
@@ -55,7 +116,7 @@ const AdminDashboard = () => {
                 <div className="card-body">
                   <h5 className="card-title text-success">Number of Members</h5>
                   <p className="card-text text-success">
-                    <i class="fa fa-users"></i> {jumlahMembers}
+                    <i class="fa fa-users"></i> {totalMembers}
                   </p>
                 </div>
                 <div className="card-footer d-flex align-items-center justify-content-between">
@@ -76,7 +137,7 @@ const AdminDashboard = () => {
                 <div className="card-body">
                   <h5 className="card-title text-success">Number of Drivers</h5>
                   <p className="card-text text-success">
-                    <i class="fa fa-car"></i> {jumlahDrivers}
+                    <i class="fa fa-car"></i> {totalDrivers}
                   </p>
                 </div>
                 <div className="card-footer d-flex align-items-center justify-content-between">
@@ -99,7 +160,7 @@ const AdminDashboard = () => {
                     Number of Partners
                   </h5>
                   <p className="card-text text-success">
-                    <i class="fa fa-building"></i> {jumlahPartners}
+                    <i class="fa fa-building"></i> {totalPartners}
                   </p>
                 </div>
                 <div className="card-footer d-flex align-items-center justify-content-between">
